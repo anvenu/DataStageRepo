36b3f593586c5804e575dbbb5d28f9ff3265c9ae
anvenu <anuvenugopal9@gmail.com>
2025-01-25 13:34:50 +0000

    Add files via upload

---
+++ execrcise2.py
@@ -0,0 +1,9 @@
+
+
+#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
+
+def bake_time_remaining(EXPECTED_BAKE_TIME):
+    return EXPECTED_BAKE_TIME - 30
+EXPECTED_BAKE_TIME=40
+BK_TIME_REMAINING=bake_time_remaining(EXPECTED_BAKE_TIME)
+print(BK_TIME_REMAINING)
